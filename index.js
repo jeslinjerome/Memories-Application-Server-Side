b8d3c76be1f3fcb87a79cfd4ec10aecf86e0fec9
Jeslin <jeslinjerome@gmail.com>
2021-12-07 15:54:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// app.use(express.json()); //use middleware to remove undefine from the console
+
+const CONNECTION_URL =
+  "mongodb+srv://jeslin:caglar0511@cluster0.eihyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false); 
+
+//database connected online on cluster atlas
+//go to: https://www.mongodb.com/cloud/atlas
