b67c3504d6b2295bc67aaf9afbc45c51caaa4ca8
jeslin <jeslinjerome@gmail.com>
2022-02-06 23:08:21 +0530

    before push

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 //every routs in postRouts is gona start with /post we addded the prefex post like "http://localhost:5000/posts"
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 // app.use(express.json()); //use middleware to remove undefine from the console
 
-const CONNECTION_URL =
-  "mongodb+srv://jeslin:caglar0511@cluster0.eihyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.MEMORIES_APP_CONNECTION_URL
 
 const PORT = process.env.PORT || 5000;
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 //like "http://localhost:5000/posts"
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost); //patch is used to update in the exixting.
 
 export default router;
 
